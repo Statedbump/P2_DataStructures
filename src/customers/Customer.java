150f850ee8d888428ef642a7874299b3045945b7
kelvin123459 <kelvin.garcia5@upr.edu>
2018-04-23 15:45:32 -0400

    Costumer class finished

--- src/customers/Customer.java
+++ src/customers/Customer.java
@@ -1,22 +1,114 @@
 package customers;
-
+/**
+ * 
+ * @author Kelvin García & Luis Cintron 
+ * Class representing the costumers in the simulation
+ */
 public class Customer {
-	private int iD; 			//used to identify the costumer
+	private int ID; 			//used to identify the costumer
 	private boolean isServed;	//true if costumer is served
 	private long arrivalTime;	//sets the time the costumer arrived
-	private long serviceTime;	//departureTime - arrivalTime
+	private long serviceTime;	//
 	private long departureTime;	//sets the time the costumer left
 	
+	/**
+	 * Default Constructor
+	 */
 	public Customer() {
 		arrivalTime = serviceTime =departureTime  = 0;
 		isServed = false;
 	}
-	
-	public Customer(int iD, long arrivalTime, long serviceTime){
-		this.iD = iD;
+	/**
+	 * Constructor
+	 * @param iD 
+	 * @param arrivalTime
+	 * @param serviceTime
+	 */
+	public Customer(int ID, long arrivalTime, long serviceTime){
+		this.ID = ID;
 		this.arrivalTime = arrivalTime;
 		this.serviceTime = serviceTime;
 		isServed = false;
+	}
+	
+	/**
+	 * 
+	 * @return boolean isServed - true if costumer has been served
+	 */
+	public boolean isServed() {
+		return isServed;
+	}
+	
+	/**
+	 * 
+	 * @param isServed
+	 */
+	public void setServed(boolean isServed) {
+		this.isServed = isServed;
+	}
+	
+	/**
+	 * Getter for costumer ID
+	 * @return int ID - different for each costumer
+	 */
+	public int getID() {
+		return ID;
+	}
+	
+	/**
+	 * Setter for costumer ID
+	 * @param ID - int to differ between costumers
+	 */
+	public void setiD(int ID) {
+		this.ID = ID;
+	}
+	
+	/**
+	 * Setter for arrivalTime
+	 * @param arrivalTime - simulates the exact time the costumer arrived
+	 */
+	public void setArrival(long arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	/**
+	 * 
+	 * @param serviceTime
+	 */
+	public void setServiceTime(long serviceTime) {
+		this.serviceTime = serviceTime;
+	}
+
+	/**
+	 * 
+	 * @param departureTime
+	 */
+	public void setDepature(long departureTime) {
+		this.departureTime = departureTime;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public long getArrival() {
+		return arrivalTime;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public long getServiceTime() {
+		return serviceTime;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public long getDeparture() {
+		return departureTime;
 	}
 	
 }
